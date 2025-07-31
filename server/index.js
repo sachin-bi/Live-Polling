f375e590277929d9c6c2a7f7008150776447c2a7
Sachinandan <sachinandanp5@gmail.com>
2025-07-31 18:31:12 +0530

    render deploy fixs

--- server/index.js
+++ server/index.js
@@ -87,7 +87,9 @@ io.on('connection', (socket) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
